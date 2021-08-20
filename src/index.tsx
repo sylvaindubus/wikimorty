880ff6e67cc040eb733f6156d599aa37f969ae47
Sylvain Dubus <svn.dbs@gmail.com>
2021-08-20 15:17:51 +0200

    Add a simple layout and change some styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,26 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-import './index.css'
 import CharacterList from './features/characterList'
 import Character from './features/character'
 import { store } from './app/store'
+import Layout from './layout'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <Switch>
-          <Route exact path="/:page?">
-            <CharacterList />
-          </Route>
-          <Route path="/character/:id">
-            <Character />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path="/:page?">
+              <CharacterList />
+            </Route>
+            <Route path="/character/:id">
+              <Character />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </Provider>
   </React.StrictMode>,
---
+++ src/layout/index.tsx
@@ -0,0 +1,72 @@
+import { ReactNode } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+const Container = styled.div`
+  text-align: center;
+  min-height: 100vh;
+  text-align: center;
+  background-color: #222f3e;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  color: #c8d6e5;
+
+  a {
+    color: #0abde3;
+  }
+`
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+`
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  padding: 30px;
+`
+
+const Main = styled.main`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1em 0;
+
+  p {
+    margin: 0.25em 0;
+  }
+`
+
+const Layout = ({ children }: LayoutProps) => (
+  <Container>
+    <Header>
+      <Link to="/">
+        <Title>Wikimorty React App</Title>
+      </Link>
+    </Header>
+    <Main>{children}</Main>
+    <Footer>
+      <p>Just a simple React app!</p>
+      <p>
+        <a href="https://github.com/sylvaindubus/wikimorty">Code source </a>
+      </p>
+    </Footer>
+  </Container>
+)
+
+export default Layout
